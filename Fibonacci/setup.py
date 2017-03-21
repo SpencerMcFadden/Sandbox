49c0a83b78adf93c5d9a98edf4ee5e199b41a3cb
Spencer McFadden <spencer.v.mcfadden@gmail.com>
2017-03-21 15:30:23 -0600

    Update setup.py
    Updated urls

--- Fibonacci/setup.py
+++ Fibonacci/setup.py
@@ -5,9 +5,9 @@ except ImportError:
 
 config = {
     'description': 'Index finder for fibonacci sequence',
-    'author': 'Spencer McFadden ',
-    'url': '',
-    'download_url': '',
+    'author': 'Spencer McFadden',
+    'url': 'https://github.com/SpencerMcFadden/Sandbox/tree/master/Fibonacci',
+    'download_url': 'https://github.com/SpencerMcFadden/Sandbox/tree/master/Fibonacci',
     'author_email': 'spencer.v.mcfadden@gmail.com',
     'version': '0.1',
     'install_requires': ['nose'],
