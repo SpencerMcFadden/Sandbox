a3ead51c47c42a47c92404c0bc12f2567e3e3855
Spencer McFadden <spencer.v.mcfadden@gmail.com>
2017-03-24 11:16:23 -0600

    Update Reader.py
    Corrected urllib.request to urllib2

--- TIL/Reader.py
+++ TIL/Reader.py
@@ -1,8 +1,8 @@
 import json
-import urllib.request
+import urllib2
 
 url = "http://www.reddit.com/r/todayilearned/new.json?sort=new"
 
-data = json.load(urllib.request.urlopen(url))
+data = json.load(urllib2.urlopen(url))
 
 print(data)
