157984f058f5451774a0e1fb964d2c8c76827e4e
Spencer McFadden <spencer.v.mcfadden@gmail.com>
2017-03-24 12:17:21 -0600

    Now parses title data of todayilearned json
    TIL isn't taken out yet

--- TIL/Reader.py
+++ TIL/Reader.py
@@ -4,5 +4,7 @@ import urllib2
 url = "http://www.reddit.com/r/todayilearned/new.json?sort=new"
 
 data = json.load(urllib2.urlopen(url))
+front = data["data"]["children"]
 
-print(data)
+for i in xrange(0, len(front)-1):
+    front[i]["data"]["title"].encode("utf-8")
