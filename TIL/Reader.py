cdc706f8dfd7723d63490cf355319297cf19e79f
Spencer McFadden <spencer.v.mcfadden@gmail.com>
2017-03-27 20:13:42 -0600

    Update conditions
    More conditions added. Now stores posts in list to be appended to text file later.

--- TIL/Reader.py
+++ TIL/Reader.py
@@ -6,5 +6,14 @@ url = "http://www.reddit.com/r/todayilearned/new.json?sort=new"
 data = json.load(urllib2.urlopen(url))
 front = data["data"]["children"]
 
+conditions = {"TIL": "", "TIL,": "", "-": "", "Til": "", "til": "", "TIL that": ""}
+conditions = dict((re.escape(k), v) for k, v in conditions.iteritems())
+pattern = re.compile("|".join(conditions.keys()))
+
 for i in xrange(0, len(front)-1):
-    print(front[i]["data"]["title"].encode("utf-8").replace("TIL","")+"\n")
+    title = front[i]["data"]["title"].encode("utf-8")
+    title = pattern.sub(lambda m: conditions[re.escape(m.group(0))], title)
+    posts.append(title+"\n")
+
+for text in posts:
+    print(text)
