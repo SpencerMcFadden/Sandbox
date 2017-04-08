2bc1a5462a6c8f48e3b229b0c0098c8f1944e894
Spencer McFadden <spencer.v.mcfadden@gmail.com>
2017-04-08 16:13:24 -0600

    Now defined functions and removes duplicate posts
    Turned 2 operations into functions and added the removeDuplicates function to prevent duplicate posts from being added to the output text file.

--- TIL/Reader.py
+++ TIL/Reader.py
@@ -1,6 +1,7 @@
 import json
 import urllib2
 import re
+import os.path
 
 url = "http://www.reddit.com/r/todayilearned/new.json?sort=new"
 
@@ -8,21 +9,40 @@ data = json.load(urllib2.urlopen(url))
 front = data["data"]["children"]
 posts = []
 
-conditions = {"TIL": "", "TIL,": "", "Til": "", "til": "", "TIL:": "", "Til:": "",\
-                "til:": "", "-": ""}
+conditions = {"TIL": "", "TIL,": "", "Til": "", "til": "", "TIL:": "",\
+                "Til:": "", "til:": "", "-": ""}
 conditions = dict((re.escape(k), v) for k, v in conditions.iteritems())
 pattern = re.compile("|".join(conditions.keys()))
 
-for i in xrange(0, len(front)-1):
-    title = front[i]["data"]["title"].encode("utf-8")
-    title = pattern.sub(lambda m: conditions[re.escape(m.group(0))], title)
-    temp = title.split(" ", 2)
-    if temp[1] == "that":
-        title = title.replace("that", "", 1)
-    title = title.strip().capitalize()
-    posts.append(title+"\n")
+def findFacts(apiFile):
+    for i in xrange(0, len(front)-1):
+        title = apiFile[i]["data"]["title"].encode("utf-8")
+        title = pattern.sub(lambda m: conditions[re.escape(m.group(0))], title)
+        temp = title.split(" ", 2)
+        if temp[1] == "that":
+            title = title.replace("that", "", 1)
+        title = title.strip().capitalize()
+        posts.append(title+"\n\n")
+    return posts
+
+def removeDuplicates(foundList):
+    tempList = []
+    for text in foundList:
+        output = open("reader.txt", "r")
+        if text in output.read(os.path.getsize("reader.txt")):
+            tempList.append(text)
+    output.close()
+    foundList = [line for line in foundList if line not in tempList]
+    return foundList
+
+def textFile(factList):
+    with open("reader.txt", "a") as output:
+        for text in factList:
+            output.write(text)
+    return output
 
 
-with open("reader.txt", "a") as output:
-    for text in posts:
-        output.write(text)
+posts = findFacts(front)
+if os.path.isfile("reader.txt"):
+    posts = removeDuplicates(posts)
+textFile(posts)
